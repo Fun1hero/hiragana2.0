d4712cf3890c60e6b7a191aa03a5102f3ebae8b5
dkab <dmitry.kabanov.a@gmail.com>
2020-08-09 15:29:31 -0700

    Light theme done, voice added

--- Hiragana 2.0/ContentView.swift
+++ Hiragana 2.0/ContentView.swift
@@ -7,9 +7,19 @@
 //
 
 import SwiftUI
+import AVFoundation
 
 extension Color {
     static let offWhite = Color.init(red: 225/255, green: 225/255, blue: 235/255)
+    static let accentColor = Color.init(red: 90/255, green: 185/255, blue: 234/255)
+    static let accentColorPurple = Color.init(red: 151/255, green: 96/255, blue: 208/255)
+    
+}
+
+extension LinearGradient {
+    init(_ colors: Color...) {
+        self.init(gradient: Gradient(colors: colors), startPoint: .leading, endPoint: .bottomTrailing)
+    }
 }
 
 struct NeuroButtonStyle: ButtonStyle {
@@ -18,13 +28,35 @@ struct NeuroButtonStyle: ButtonStyle {
     func makeBody(configuration: Configuration) -> some View {
         configuration.label
             .padding(20)
+            .frame(width: self.width, height: self.height)
             .contentShape(RoundedRectangle(cornerRadius: 25))
+            
             .background(
                 Group {
                     if (configuration.isPressed){
+                        
                         RoundedRectangle(cornerRadius: 25)
                             .fill(Color.offWhite)
                             .frame(width: self.width, height: self.height)
+                            .overlay(
+                                RoundedRectangle(cornerRadius: 25)
+                                    .stroke(Color.gray, lineWidth: 4)
+                                    .blur(radius: 4)
+                                    .offset(x: 2, y:2)
+                                    .mask(
+                                        RoundedRectangle(cornerRadius: 25).fill(LinearGradient(Color.black, Color.clear))
+                                )
+                                
+                        )
+                            .overlay(
+                                RoundedRectangle(cornerRadius: 25)
+                                    .stroke(Color.white, lineWidth: 4)
+                                    .blur(radius: 4)
+                                    .offset(x: -2, y:-2)
+                                    .mask(
+                                        RoundedRectangle(cornerRadius: 25).fill(LinearGradient(Color.clear, Color.black))
+                                )
+                        )
                     } else {
                         RoundedRectangle(cornerRadius: 25)
                             .fill(Color.offWhite)
@@ -38,17 +70,35 @@ struct NeuroButtonStyle: ButtonStyle {
 }
 
 struct NeuroButtonCircleStyle: ButtonStyle {
-//    @State var width: CGFloat
-//    @State var height: CGFloat
+    @State var pressed: Bool?
     func makeBody(configuration: Configuration) -> some View {
         configuration.label
             .padding(20)
             .contentShape(Circle())
             .background(
                 Group {
-                    if (configuration.isPressed){
+                    if (pressed ?? configuration.isPressed){
                         Circle()
                             .fill(Color.offWhite)
+                            .overlay(
+                                Circle()
+                                    .stroke(Color.gray, lineWidth: 4)
+                                    .blur(radius: 4)
+                                    .offset(x: 2, y:2)
+                                    .mask(
+                                        Circle().fill(LinearGradient(Color.black, Color.clear))
+                                )
+                                
+                        )
+                            .overlay(
+                                Circle()
+                                    .stroke(Color.white, lineWidth: 8)
+                                    .blur(radius: 4)
+                                    .offset(x: -2, y:-2)
+                                    .mask(
+                                        Circle().fill(LinearGradient(Color.clear, Color.black))
+                                )
+                        )
                     } else {
                         Circle()
                             .fill(Color.offWhite)
@@ -67,23 +117,42 @@ struct ContentView: View {
     
     @State var inOrder :Bool = true;
     @State var challange :Bool = false;
-    @State var spelling :Bool = false;
+    @State var spelling :Bool = true;
     
     @State var selectedKana :Int = 0
     
     @Environment(\.colorScheme) var colorScheme
     
     
+    let synthesizer = AVSpeechSynthesizer()
+    
+    func spellKana(){
+        let utterance = AVSpeechUtterance(string: self.hiraganaSequenceToRead[self.selectedKana])
+        utterance.voice = AVSpeechSynthesisVoice(language: "ja-JP")
+        utterance.rate = 0.2
+        utterance.volume = 1.4
+        
+//        if self.synthesizer.isSpeaking {
+//            self.synthesizer.stopSpeaking(at: AVSpeechBoundary(rawValue: 0)!)
+//        } else {
+            self.synthesizer.speak(utterance)
+//        }
+    }
+    
     func nextKana(next: Bool)
     {
         if (self.inOrder){
             if(next){
-                if (self.selectedKana > 0){
-                    self.selectedKana -= 1
-                }
-            } else {
                 if (self.selectedKana < self.hiraganaSequence.capacity-1){
                     self.selectedKana += 1
+                } else {
+                    self.selectedKana = 0
+                }
+            } else {
+                if (self.selectedKana > 0){
+                    self.selectedKana -= 1
+                } else {
+                    self.selectedKana = self.hiraganaSequence.capacity-1
                 }
             }
         } else {
@@ -94,72 +163,83 @@ struct ContentView: View {
     var body: some View {
         ZStack {
             Color.offWhite
-            //        NavigationView {
             
-            VStack (spacing: 40) {
-                HStack (spacing: 45){
+            VStack (spacing: 30) {
+                HStack (spacing: 30){
                     Button(action: {
-                        self.nextKana(next: true)
+                        self.nextKana(next: false)
+                        if self.spelling {
+                            self.spellKana()
+                        }
                     }){
+                        
                         Image(systemName: "chevron.left")
-                        .foregroundColor(.gray)
-//                        Image(systemName: "play.fill").rotationEffect(.degrees(180))
+                            .foregroundColor(.gray)
                     }.buttonStyle(NeuroButtonStyle(width: 60, height: 250))
                     
                     
-                    Button(action: {print("play voice")}){
+                    Button(action: {
+                        if self.spelling {
+                            self.spellKana()
+                        }
+                    }){
                         Text(self.hiraganaSequenceToRead[self.selectedKana])
                             .font(.system(size: 130, weight: .light))
-                            .foregroundColor(.gray)
+                            .foregroundColor(.accentColor)
                     }.buttonStyle(NeuroButtonStyle(width: 200, height: 250))
                     
                     Button(action: {
-                        self.nextKana(next: false)
+                        self.nextKana(next: true)
+                        if self.spelling {
+                            self.spellKana()
+                        }
                     }){
                         Image(systemName: "chevron.right")
-                        .foregroundColor(.gray)
+                            .foregroundColor(.accentColorPurple)
                     }.buttonStyle(NeuroButtonStyle(width: 60, height: 250))
                 }
                 
                 
-                
-                Text(self.hiraganaSequence[self.selectedKana])
+                Text(self.challange ? " " : self.hiraganaSequence[self.selectedKana])
                     .font(.system(size: 50, weight: .light))
                     .foregroundColor(.gray)
                     .padding(.top, 40)
-//                    .padding(.top)
-
                 
-                HStack (spacing: 20){
+                HStack (spacing: 46){
                     Button(action: {
                         self.inOrder.toggle()
                     }){
                         Image(systemName: "textformat.abc.dottedunderline")
-                        .foregroundColor(.gray)
-                    }.buttonStyle(NeuroButtonCircleStyle())
+                            .foregroundColor(.accentColorPurple)
+                    }.buttonStyle(NeuroButtonCircleStyle(pressed: !self.inOrder))
                     
                     Button(action: {
-                        print("order")
+                        print("challange")
+                        self.challange.toggle()
+                        self.spelling = false
                         
                     }){
                         Image(systemName: "studentdesk")
-                        .foregroundColor(.gray)
-                    }.buttonStyle(NeuroButtonCircleStyle())
+                            .foregroundColor(.accentColorPurple)
+                    }.buttonStyle(NeuroButtonCircleStyle(pressed: self.challange))
                     
                     
                     Button(action: {
-                        print("order")
-                        
+                        if self.challange {
+                            self.spelling = false
+                        } else {
+                            self.spelling.toggle()
+                        }
                     }){
                         Image(systemName: "speaker.2.fill")
-                        .foregroundColor(.gray)
-                    }.buttonStyle(NeuroButtonCircleStyle())
+                            .foregroundColor(.accentColorPurple)
+                    }.buttonStyle(NeuroButtonCircleStyle(pressed: spelling))
                     
                     Button(action: {
                         self.selectedKana = 0
                     }){
                         Image(systemName: "arrow.counterclockwise")
-                        .foregroundColor(.gray)
+                            .foregroundColor(.accentColorPurple)
                     }.buttonStyle(NeuroButtonCircleStyle())
                 }
                 .padding()
@@ -167,10 +247,9 @@ struct ContentView: View {
                 
             }.navigationBarTitle("Hiragana")
         }.edgesIgnoringSafeArea(.all)
-        //        }
     }
 }
-    
+
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
